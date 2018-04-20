cc9816e662f61ece4193f44129793a4c38af7e17
Zac Lucarelli <chikanz@outlook.com>
2018-04-20 17:09:42 +1000

    Initial commit :tada:

---
+++ Assets/Scripts/AttackMessageArgument.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// Used as the argument for an Attack message. Contains information
+/// regarding an attack performed on a GameObject by another.
+/// </summary>
+public class AttackMessageArgument : Object
+{
+	// Properties
+
+	/// <summary>
+	/// How strong of an attack this is.
+	/// </summary>
+	public int DamageStrength { get; private set; }
+
+	/// <summary>
+	/// What kind of damage is does this attack do?
+	/// </summary>
+	public AttackType DamageType { get; private set; }
+
+	/// <summary>
+	/// Who attacked us?
+	/// </summary>
+	public GameObject Attacker { get; private set; }
+
+
+    /// <summary>
+    /// Where we got hit (optional)
+    /// </summary>
+    public Vector3 hitPoint;
+
+    /// <summary>
+    /// Override hit particles
+    /// </summary>
+    public BaseHealth.EHitParticles forceHit;
+
+    // Contructors
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AttackMessageArgument"/> class.
+    /// </summary>
+    /// <param name="attStrength">Strength of the attack</param>
+    /// <param name="attType">Type of the attack</param>
+    /// <param name="attFrom">Who did this?</param>
+    public AttackMessageArgument(int attStrength, AttackType attType, GameObject attFrom)
+	{
+		DamageStrength = attStrength;
+		DamageType = attType;
+		Attacker = attFrom;
+	}
+
+
+    //Override for hit position
+    public AttackMessageArgument(int attStrength, AttackType attType, GameObject attFrom, Vector3 hitpos)
+    {
+        DamageStrength = attStrength;
+        DamageType = attType;
+        Attacker = attFrom;
+        hitPoint = new Vector3(hitpos.x,hitpos.y,hitpos.z);
+    }
+
+
+    //Override for force hit particles + position
+    public AttackMessageArgument(int attStrength, AttackType attType, GameObject attFrom, Vector3 hitpos, BaseHealth.EHitParticles forceParticles)
+    {
+        DamageStrength = attStrength;
+        DamageType = attType;
+        Attacker = attFrom;
+        hitPoint = new Vector3(hitpos.x, hitpos.y, hitpos.z);
+        forceHit = forceParticles;
+    }
+}
+
---
+++ Assets/Scripts/AttackType.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Defines what type of attack is being inflicted.
+/// NOTE: New magic damage types MUST be added to the Magic flag. Problems will
+/// occur if this isn't done.
+/// </summary>
+[Flags]
+public enum AttackType
+{
+	/// <summary>
+	/// No attack type. Used for defense specifications.
+	/// </summary>
+	[Tooltip("No attack type.")]
+	None 		= 0,
+
+	/// <summary>
+	/// Damage from getting shot by an arrow.
+	/// </summary>
+	[Tooltip("The physical damage of being hit with an arrow")]
+	Arrow 		= 1 << 0,
+
+	/// <summary>
+	/// Damage cause by a nearby explosion.
+	/// </summary>
+	[Tooltip("The physical damage of being too close to an explosion")]
+	Explosion 	= 1 << 1,
+
+	/// <summary>
+	/// Damage caused by being pushed.
+	/// </summary>
+	[Tooltip("The physical damage of being pushed by something")]
+	Pushback 	= 1 << 2,
+
+	/// <summary>
+	/// Damage cause by an axe
+	/// </summary>
+	[Tooltip("The physical damage of being hit with an axe")]
+	Axe			= 1 << 3,
+
+	/// <summary>
+	/// Damage cause by a club
+	/// </summary>
+	[Tooltip("The physical damage of being hit with a club")]
+	Club		= 1 << 4,
+
+	/// <summary>
+	/// Damage cause by poop? Who throws poop?
+	/// </summary>
+	[Tooltip("The physical damage- seriously, who throws poop?")]
+	Poo			= 1 << 5,
+
+	/// <summary>
+	/// Damage caused by flames
+	/// </summary>
+	[Tooltip("The magical damage of being burnt")]
+	Fire 		= 1 << 6,
+
+	/// <summary>
+	/// Damage caused by static discharge
+	/// </summary>
+	[Tooltip("The magical damage of being shocked")]
+	Lightning 	= 1 << 7,
+
+	/// <summary>
+	/// Damage cause by chilled water
+	/// </summary>
+	[Tooltip("The magical damage of being blizzard'ed")]
+	Ice			= 1 << 8,
+
+	/// <summary>
+	/// Damage caused by the very ground itself!
+	/// </summary>
+	[Tooltip("The magical damage of being earthquaked")]
+	Earth		= 1 << 9,
+
+	/// <summary>
+	/// Damage caused by any form of magic
+	/// </summary>
+	[Tooltip("The magic damage of magic")]
+	Magic		= Fire | Lightning | Ice | Earth,
+
+    /// <summary>
+    /// Sent when player scores a crit, usually via arrow headshot
+    /// </summary>
+    [Tooltip("Critical Hit!")]
+    Critical = 1 << 10,
+}
