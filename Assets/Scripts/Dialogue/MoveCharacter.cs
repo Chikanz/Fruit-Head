ecb795fd9407b38f7ba1055a5c8e55157966efcc
amy <38675419+amyd1@users.noreply.github.com>
2018-05-21 18:45:03 +1000

    Dialogue changes
    added some more npcs including party member
    implemented basic move and activate functionality

---
+++ Assets/Scripts/Dialogue/MoveCharacter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Yarn.Unity {
+	public class MoveCharacter : MonoBehaviour {
+
+		GameObject target;
+		float speed;
+		public GameObject dialogue;
+
+
+		// Use this for initialization
+		void Start () {
+			target = null;
+			speed = 2.0f;
+
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			if (target != null ) {
+
+				float step = speed * Time.deltaTime;
+				Transform destination = target.gameObject.transform;
+				transform.position = Vector3.MoveTowards(transform.position, destination.position, step);
+
+				if (Vector3.Distance (transform.position, destination.position) < 2.0f) {
+					if (target.GetComponent<OW_Character> ().Name == "Charlie") {
+						string startNode = gameObject.GetComponent<OW_NPC> ().StartNode;
+						dialogue.GetComponent<DialogueRunner> ().StartDialogue (startNode);
+					}
+					target = null;
+				}
+			}
+
+		}
+
+
+		[YarnCommand("move")]
+		public void movetopoint(string destination) {
+
+			print (destination);
+			target = GameObject.Find (destination);
+
+			print (target.name);
+
+		}
+
+	}
+}
