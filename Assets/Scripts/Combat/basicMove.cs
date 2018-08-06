07df80acaed3c02f68a0452be751ed6a9f3d495e
Mish Forder <mishd14@gmail.com>
2018-08-07 00:36:06 +1000

    MAde rigged/animator prefabs for dog/wolves, Added wolf run animation but having issues with it functioning

--- Assets/Scripts/Combat/basicMove.cs
+++ Assets/Scripts/Combat/basicMove.cs
@@ -9,23 +9,33 @@ public class basicMove : MonoBehaviour {
     public Transform[] points;
     NavMeshAgent NM;
     int counter = 0;
+ 
 
     // Use this for initialization
     void Start () {
 
         NM = GetComponent<NavMeshAgent>();
         NM.SetDestination(points[counter].position);
+    
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+
         if (Vector3.Distance(transform.position, points[counter].position) < 0.5f)
         {
             
             counter++;
             if (counter > points.Length - 1) counter = 0;
             NM.SetDestination(points[counter].position);
+
+          
         }
 	}
+
+
+
+
 }
+
