d6cfccac291d041395e30c55d0ba693c38615ddc
amyd1 <38675419+amyd1@users.noreply.github.com>
2018-08-19 17:22:29 +1000

    started berry island dialogue integration

---
+++ Assets/Scripts/Dialogue/startDialogue.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+using UnityEngine.SceneManagement;
+
+public class startDialogue : MonoBehaviour {
+
+    static GameObject dialogue;
+    public string scene;
+
+
+    // Use this for initialization
+    void Start () {
+
+        if (!dialogue) dialogue = GameObject.Find("Yarn");
+        if (gameObject.name != "MeetingTrigger")
+        {
+            runStartDialogue();
+        }
+        
+
+    }
+	
+	void runStartDialogue()
+    {
+        Yarn.Value biStage = dialogue.GetComponent<ExampleVariableStorage>().GetValue("$biStage");
+        float stage = biStage.AsNumber;
+        if (scene == "BerryIsland")
+        {
+            if (stage == 1)
+            {
+                dialogue.GetComponent<DialogueRunner>().StartDialogue("BerryIslandStart");
+            }
+            else if (stage == 1.5)
+            {
+                dialogue.GetComponent<DialogueRunner>().StartDialogue("KellsHouseExit");
+            }
+        }
+        else if (scene == "KellsHouse")
+        {
+            if (stage == 1)
+            {
+                dialogue.GetComponent<DialogueRunner>().StartDialogue("KellsHouseInt");
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Yarn.Value biStage = dialogue.GetComponent<ExampleVariableStorage>().GetValue("$biStage");
+        float stage = biStage.AsNumber;
+        print("debate");
+        if (stage == 1.5)
+        {
+            dialogue.GetComponent<DialogueRunner>().StartDialogue("Debate");
+        }
+    }
+
+}
